9c269f5f7045f6881f5fdd3d033a394aed67927a
hagerlabib <hageerlabib@gmail.com>
2023-05-02 12:46:09 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-        while (s[i])
+	while (s[i])
 		i++;
 
 	while (i--)
-        {
-                _putchar(s[i]);
-        }
-        _putchar('\n');
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
