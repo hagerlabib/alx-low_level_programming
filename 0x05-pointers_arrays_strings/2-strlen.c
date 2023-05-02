e9a4aa9d3cc216b3823a2158211457d6b1584c86
hagerlabib <hageerlabib@gmail.com>
2023-05-02 10:04:22 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns the length of a string
+ * @s: input parameter
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for(counter=0; *s != '\0'; s++)
+		++counter
+			
+	return (counter);
+}
+
